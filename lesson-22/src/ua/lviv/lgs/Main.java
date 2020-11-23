f451062df51019a66156a7381739d7353c74e163
Eugene <Eugene@DESKTOP-RJH8CQD>
2020-11-23 23:58:06 +0200

    added twenty first task

--- lesson-22/src/ua/lviv/lgs/Main.java
+++ lesson-22/src/ua/lviv/lgs/Main.java
@@ -2,14 +2,18 @@ package ua.lviv.lgs;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		Pet cow = () -> System.out.println("ß êîðîâà- Ìóóó-Ìóóó");
 		cow.voice();
 		Pet cat = () -> System.out.println("ß ê³ò- Ìÿóóó-Ìÿóóó");
 		cat.voice();
 		Pet dog = () -> System.out.println("ß ïåñ - Ãàóóó-Ãàóóó");
 		dog.voice();
+		
+		System.out.println();
+		
+		OverloadedConstructor overloadedConstructor = ClassWithTwoOverloadedConstructors::new;
+		overloadedConstructor.overloaded(100);
 		
 		System.out.println();
 		
@@ -20,8 +24,7 @@ public class Main {
 		Amphibia frogSwimming = () -> System.out.println("I am swimming");
 		frogDoSomething(frogSwimming);		
 		Amphibia frogWalking = () -> System.out.println("I am walking");
-		frogDoSomething(frogWalking);
-		
+		frogDoSomething(frogWalking);		
 	}
 	
 	public static void frogDoSomething(Amphibia whatToDo) {
@@ -30,12 +33,28 @@ public class Main {
 
 }
 
+//1 task
 interface Pet {	
 	public void voice();
 }
 
-interface Amphibia {
+//2 task
+interface OverloadedConstructor{
+	ClassWithTwoOverloadedConstructors overloaded(int i);
+}
+
+class ClassWithTwoOverloadedConstructors {
+	public ClassWithTwoOverloadedConstructors() {
+		System.out.println("ClassWithTwoOverloadedConstructors()");
+	}
 	
-	public void doSomething();
-	
+	public ClassWithTwoOverloadedConstructors(int i) {
+		this();
+		System.out.println("ClassWithTwoOverloadedConstructors(int " + i + ")");
+	}	
+}
+
+//3 task
+interface Amphibia {	
+	public void doSomething();	
 }
